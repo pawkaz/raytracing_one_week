6e605e994f16b9a8e72e995ce00339682490e831
paka <pawel.kaz@pm.me>
2020-12-06 16:59:05 +0100

    chapter 4

--- vec3.h
+++ vec3.h
@@ -89,7 +89,7 @@ inline vec3 operator+(const vec3& v1, const vec3& v2) {
   return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
 
-inline vec3 operator-(vec3& v1, vec3& v2) {
+inline vec3 operator-(const vec3& v1,const vec3& v2) {
   return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
 }
 
@@ -118,7 +118,7 @@ inline vec3 operator*(const vec3& v1, const float t) {
 }
 
 inline float dot(const vec3& v1, const vec3& v2) {
-  return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[2] + v1.e[2] * v2.e[2];
+  return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 }
 
 inline vec3 cross(const vec3& v1, const vec3& v2) {
