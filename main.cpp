b60ddaa93f87cde5af5f8ac3853bceaa486922ab
paka <pawel.kaz@pm.me>
2020-12-10 10:25:29 +0100

    chapter 9

--- main.cpp
+++ main.cpp
@@ -40,8 +40,8 @@ vec3 color(const ray& r, hitable* world, int depth) {
 }
 
 int main() {
-  int nx = 400;
-  int ny = 200;
+  int nx = 200;
+  int ny = 100;
   int ns = 100;
 
   std::ofstream myfile;
@@ -55,10 +55,10 @@ int main() {
   vec3 origin(0., 0., 0.);
 
   hitable* list[4];
-  list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+  list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
   list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0)));
-  list[2] = new sphere(vec3(1, 0, -1), .5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-  list[3] = new sphere(vec3(-1, 0, -1), .5, new metal(vec3(0.8, 0.8, 0.8), 1.0));
+  list[2] = new sphere(vec3(1, 0, -1), .5, new metal(vec3(0.8, 0.6, 0.2)));
+  list[3] = new sphere(vec3(-1, 0, -1), .5, new dielectric(1.5));
   hitable* world = new hitable_list(list, 4);
   camera cam;
 
