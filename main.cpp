565358667444773c9d3ef8ea43cadc32c5a2b6cc
paka <pawel.kaz@pm.me>
2020-12-11 08:26:58 +0100

    chapter 10

--- main.cpp
+++ main.cpp
@@ -9,16 +9,50 @@
 #include "vec3.h"
 #include "material.h"
 
-// vec3 random_in_unit_sphere() {
-//   vec3 p;
-//   std::random_device rd;
-//   std::mt19937 gen(rd());
-//   std::uniform_real_distribution<float> dis(0, 1);
-//   do {
-//     p = 2.0 * vec3(dis(gen), dis(gen), dis(gen)) - vec3(1, 1, 1);
-//   } while (p.squared_length() >= 1.0);
-//   return p;
-// }
+
+hitable *random_scene() {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<float> dis(0, 1);
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = dis(gen);
+            vec3 center(a+0.9*dis(gen),0.2,b+0.9*dis(gen));
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new lambertian(vec3(dis(gen)*dis(gen),
+                                            dis(gen)*dis(gen),
+                                            dis(gen)*dis(gen)))
+                    );
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new metal(vec3(0.5*(1 + dis(gen)),
+                                       0.5*(1 + dis(gen)),
+                                       0.5*(1 + dis(gen))),
+                                  0.5*dis(gen))
+                    );
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list,i);
+}
 
 vec3 color(const ray& r, hitable* world, int depth) {
   hit_record rec;
@@ -40,8 +74,8 @@ vec3 color(const ray& r, hitable* world, int depth) {
 }
 
 int main() {
-  int nx = 200;
-  int ny = 100;
+  int nx = 800;
+  int ny = 400;
   int ns = 100;
 
   std::ofstream myfile;
@@ -54,13 +88,19 @@ int main() {
   vec3 vertical(0., 2., 0.);
   vec3 origin(0., 0., 0.);
 
-  hitable* list[4];
-  list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-  list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0)));
-  list[2] = new sphere(vec3(1, 0, -1), .5, new metal(vec3(0.8, 0.6, 0.2)));
-  list[3] = new sphere(vec3(-1, 0, -1), .5, new dielectric(1.5));
-  hitable* world = new hitable_list(list, 4);
-  camera cam;
+  // hitable* list[4];
+  // list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
+  // list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0)));
+  // list[2] = new sphere(vec3(1, 0, -1), .5, new metal(vec3(0.8, 0.6, 0.2)));
+  // list[3] = new sphere(vec3(-1, 0, -1), .5, new dielectric(1.5));
+  // hitable* world = new hitable_list(list, 4);
+  hitable* world = random_scene();
+
+  vec3 lookfrom(13,2,3);
+  vec3 lookat(0,0,0);
+  float dist_to_focus = 10.0;
+  float aperture = 0.1;
+  camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
 
   std::random_device rd;
   std::mt19937 gen(rd());
@@ -88,7 +128,7 @@ int main() {
   }
   myfile.close();
   delete world;
-  delete list[0];
-  delete list[1];
+  // delete list[0];
+  // delete list[1];
   return 0;
 }
